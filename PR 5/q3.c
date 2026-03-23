4e2d88806061a7af98f8a560688549fca24131d2
Jaymit Parmar <jaymitparmar25@gmail.com>
2026-03-23 15:03:55 +0530

    PR 5
    PR 5

---
+++ PR 5/q3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include<conio.h>
+
+main()
+{
+    int r, c;
+      clrscr();
+
+    printf("Enter array row size :");
+    scanf("%d", &r);
+
+    printf("Enter array column size :");
+    scanf("%d", &c);
+
+    int p [r][c];
+    printf("\nEnter array elements :\n");
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("Enter p [%d][%d] :", i, j);
+            scanf("%d", &p[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n Transpose matrix of  array :\n\n");
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", p[j][i]);
+        }
+        printf("\n");
+    }
+   getch();
+}
